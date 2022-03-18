74d67eb5cc5ca3527644984d09aaabb837b486f3
watanabe <chilling22sado@gmail.com>
2022-03-18 20:27:58 +0900

    チェック

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import StoreProvider from './store/index'
 
+// StoreProviderで囲む
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider>
